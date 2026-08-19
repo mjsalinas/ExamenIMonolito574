26cdc31504d91773ee586905189d490b8cf37b72
acostaanton2003-afk <anton.acosta@unitec.edu>
2026-08-18 19:43:28 -0600

    migracion

---
+++ Migrations/20260819012414_Init.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RefugioMascotas.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cuidadores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Turno = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cuidadores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mascotas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Especie = table.Column<string>(type: "TEXT", nullable: false),
+                    Edad = table.Column<int>(type: "INTEGER", nullable: false),
+                    EnTratamiento = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CuidadorId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mascotas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Mascotas_Cuidadores_CuidadorId",
+                        column: x => x.CuidadorId,
+                        principalTable: "Cuidadores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mascotas_CuidadorId",
+                table: "Mascotas",
+                column: "CuidadorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Mascotas");
+
+            migrationBuilder.DropTable(
+                name: "Cuidadores");
+        }
+    }
+}
---
+++ Migrations/20260819012556_NuevaMigracion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RefugioMascotas.Migrations
+{
+    /// <inheritdoc />
+    public partial class NuevaMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
