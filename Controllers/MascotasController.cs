456647a23fcfa2b5d240bcafecaed9443e2d9aed
PACHECO <mauripache44@gmail.com>
2026-08-18 20:20:47 -0600

    se corrigio condicion de validacion de rango

--- Controllers/MascotasController.cs
+++ Controllers/MascotasController.cs
@@ -35,9 +35,8 @@ public class MascotasController : ControllerBase
             return BadRequest("La especie es obligatoria.");
 
         // Un voluntario reportó que esta validación de edad se comporta raro (Ticket 0)
-        if (mascota.Edad > 0 || mascota.Edad > 30)
-            return BadRequest("La edad debe estar entre 0 y 30 años.");
-
+        if (mascota.Edad < 0 || mascota.Edad > 30)
+    return BadRequest("La edad debe estar entre 0 y 30 años.");
         var cuidadorExiste = await _db.Cuidadores.AnyAsync(c => c.Id == mascota.CuidadorId);
         if (!cuidadorExiste)
             return BadRequest("El cuidador especificado no existe.");
