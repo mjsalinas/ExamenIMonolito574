274220adf75b573d7265d50dfbfcc5f69f0f2ce0
TaniaOliva <taniaoliva1214@gmail.com>
2026-08-18 20:50:27 -0600

    tiquet 0 se corriguio la validacion del rango

--- Controllers/MascotasController.cs
+++ Controllers/MascotasController.cs
@@ -36,8 +36,10 @@ public class MascotasController : ControllerBase
             return BadRequest("La especie es obligatoria.");
 
         // Un voluntario reportó que esta validación de edad se comporta raro (Ticket 0)
-        if (mascota.Edad > 0 || mascota.Edad > 30)
+        if (mascota.Edad <= 0 || mascota.Edad > 30)
             return BadRequest("La edad debe estar entre 0 y 30 años.");
+
+        
 
         var cuidadorExiste = await _db.Cuidadores.AnyAsync(c => c.Id == mascota.CuidadorId);
         if (!cuidadorExiste)
